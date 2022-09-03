99c1887f30aeb54a331f0a1b3f358828be0e5e61
Mihai Moldovan <mihaiflorinmoldovan@yahoo.com>
2022-09-03 19:13:34 +0300

    todo(react): wasting time trying to render objects later, see `IMPORTANT`

--- showcase-ideas/src/components/Card.js
+++ showcase-ideas/src/components/Card.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 
 // help: [ https://stackoverflow.com/questions/41064718/how-to-create-an-new-instance-of-an-object-in-react ] - precisely what is needed
 // help: [ https://create-react-app.dev/docs/importing-a-component/ ] - fix import
@@ -12,7 +12,7 @@ const Card = (props) => {
 
     // constructor(props) {
         // super(props) // help: [ https://stackoverflow.com/questions/31067368/how-to-extend-a-class-without-having-to-use-super-in-es6 ]
-        console.log(props) // help: [ https://stackoverflow.com/questions/40433463/what-does-calling-super-in-a-react-constructor-do ]
+        console.log("card: " + this.props) // help: [ https://stackoverflow.com/questions/40433463/what-does-calling-super-in-a-react-constructor-do ]
         // this.state = {
         //     value: null,
         //   }
@@ -33,7 +33,7 @@ const Card = (props) => {
     // render() {
       // help: [ https://stackoverflow.com/questions/33766085/how-to-avoid-extra-wrapping-div-in-react ]
         return (
-          <b>Hello !</b>
+          <Fragment><b>Hello {this.props.title}!</b></Fragment>
           // <Fragment>
           //   <Text>Hello, I am your cat!</Text>
           //   <div className="card">
--- showcase-ideas/src/components/Grid.js
+++ showcase-ideas/src/components/Grid.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 
 // const Grid = (props) => {
 //     // help: [ https://reactjs.org/docs/jsx-in-depth.html ] - need to nest multiple layers of code and html
@@ -23,40 +23,68 @@ import React from 'react';
 //     )
 // }
 
+const Cell = (props) => {
+    return (
+        <td>{props.item}</td>
+    )
+}
+
 // TODO: figure out why react has become so finger sticky, basically cannot do a simple FOR in FOR and add ELEMENT, really sad
 // help: [ https://bobbyhadz.com/blog/react-get-key-of-clicked-element ]
 // help: [ https://codesandbox.io/s/react-hooks-change-list-view-to-grid-with-saving-state-qdej4?from-embed ] - exactly what I need, however trying to avoid bootstrap
 // help: [ https://medium.com/@kalebjdavenport/how-to-create-a-grid-layout-in-react-native-7948f1a6f949 ] - not what I need, basically cannot decide on custom grid
 // help: [ https://snack.expo.dev/ ] - another example, but not what I need, I need dynamically controlled grids, not FLEX_BOXES that change depending on CSS attributes
 const Grid = (props) => {
-    let AGrid = []
+    let AGrid = {}
     for(let i=0; i<props.items.length;) {
-        let ARow = []
+        let ARow = {}
         for(let col = 0; col<props.cols; col++) {
             if (i < props.items.length) {
                 // help: [ https://bobbyhadz.com/blog/javascript-typeerror-push-is-not-a-function ] - push() requires an array parameter
                 // help: [ https://reactjs.org/docs/react-without-jsx.html ] - need to avoid jsx syntax when nesting multiple elements
-                let o1 = props.items[i]
-                console.log("o1: " + o1)
+                let O1 = props.items[i]
+                console.log("O1: " + O1)
                 // console.log("o1: " + JSON.stringify(o1, null, 2))
-                let o2 = <o1 key={i} />
-                console.log("o2: " + o2)
+                let O2 = <o1/>
+                console.log("O2: " + O2)
+                let O3 = <td key={i}>{O2}</td>
+                // let o3 = React.createElement('td', null, o2)
+                console.log("O3: " + O3)
+                // console.log("o3: " + JSON.stringify(o3, null, 2))
+                // let o3 = <td>{o2}<td/>
                 // console.log("o2: " + JSON.stringify(o2, null, 2))
-                ARow += React.createElement('td', null, o2)
+                // ARow += <O3/><td key={i}><O1/></td>
+                ARow += <Cell item={props.items[i]} />
                 i ++;
             } else {
                 break
             }
         }
         // AGrid.push(ARow.map(t => <span>{t}</span>).reduce((prev, curr) => [prev, ', ', curr]))
-        AGrid.push(ARow)
+        AGrid += <tr><ARow /></tr>
         // help: [ https://stackoverflow.com/questions/30765163/pretty-printing-json-with-react ] - using JSON to print string objects
-        console.log("Row: " + JSON.stringify(ARow, null, 2))
+        // console.log("Row: " + JSON.stringify(ARow, null, 2))
     }
-    console.log("Grid: " + JSON.stringify(AGrid, null, 2))
+    // console.log("Grid: " + JSON.stringify(AGrid, null, 2))
 
     // return ("Grid")
-    return (<table>{AGrid.map(t => <span>{t}</span>).reduce((prev, curr) => [prev, ', ', curr])}</table>)
+    // return (<table>{AGrid.map(t => <Fragment>{t}</Fragment>).reduce((prev, curr) => [prev, ', ', curr])}</table>)
+    // return (<table>{AGrid.map(t => <Fragment>{t}</Fragment>)}</table>)
+    // return (<table><AGrid /></table>)
+    let cards = props.items
+    // help: [ https://stackoverflow.com/questions/60319702/how-to-render-element-in-react-js-from-object-map ]
+    return (
+        <table>
+            {/* {cards.map(card, i) => (
+                if (i % props.cols == 0) <tr><td>{card}</td>;
+                if (i % props.cols == props.cols - 1) <td>{card}</td></tr>;
+            )} */}
+            {/* <tr><td>{cards[0].renderElement()}</td><td>{cards[1].renderElement()}</td></tr>
+            <tr><td>{cards[2].renderElement()}</td><td>{cards[3].renderElement()}</td></tr>
+            <tr><td>{cards[4].renderElement()}</td><td></td></tr> */}
+        </table>
+    )
+
 }
 
 export default Grid;
