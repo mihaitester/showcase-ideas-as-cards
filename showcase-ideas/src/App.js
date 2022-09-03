36b4a105b469218c2d7d5680daf6e1696346b808
Mihai Moldovan <mihaiflorinmoldovan@yahoo.com>
2022-09-03 11:59:31 +0300

    todo(card): need to figure out how to make the `Card` class similar to that of `react-bootstrap`

--- showcase-ideas/src/App.js
+++ showcase-ideas/src/App.js
@@ -1,6 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
 
+import Card from './Card.js'; // help: [ https://bobbyhadz.com/blog/react-element-type-is-invalid-expected-string-but-got ] - use plain import for Class, use `{}` import for functions
+
+// help: [ https://react-bootstrap.github.io/components/cards/ ] - this is one the ways to present this idea, like `Magic the Gathering` cards
+// help: [ https://create-react-app.dev/docs/adding-custom-environment-variables ] - perhaps use environment variables to define behaviour of application - like path to folder containing ideas
+
 function App() {
   return (
     <div className="App">
@@ -17,6 +22,7 @@ function App() {
         >
           Learn React
         </a>
+        <Card />
       </header>
     </div>
   );
