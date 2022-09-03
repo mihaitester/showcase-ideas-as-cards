6138e1b816692b19a2042bccbdb9cda4b9b00b73
Mihai Moldovan <mihaiflorinmoldovan@yahoo.com>
2022-09-03 13:16:46 +0300

    todo(): avoid using objects since `react` is moving in the direction of functional programming with hooks

--- showcase-ideas/src/App.js
+++ showcase-ideas/src/App.js
@@ -1,7 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 
-import Card from './Card.js'; // help: [ https://bobbyhadz.com/blog/react-element-type-is-invalid-expected-string-but-got ] - use plain import for Class, use `{}` import for functions
+import Card from './components/Card'; // help: [ https://bobbyhadz.com/blog/react-element-type-is-invalid-expected-string-but-got ] - use plain import for Class, use `{}` import for functions
 
 // help: [ https://react-bootstrap.github.io/components/cards/ ] - this is one the ways to present this idea, like `Magic the Gathering` cards
 // help: [ https://create-react-app.dev/docs/adding-custom-environment-variables ] - perhaps use environment variables to define behaviour of application - like path to folder containing ideas
@@ -24,7 +24,7 @@ function App() {
         </a>
         {/* todo: need a parsing method for the subfolder containing ideas, based on the values returned by that, populate a grid of cards  */}
         {/* help: [ https://reactjs.org/docs/components-and-props.html ] */}
-        <Card title="" image="" url="" text="" tags=""/>
+        <Card title="something" image="" url="" text="" tags=""/>
         {/* todo: remember when card was clicked, display it underneath the card, perhaps change background based on how long ago link was clicked */}
       </header>
     </div>
---
+++ showcase-ideas/src/components/Card.js
@@ -0,0 +1,56 @@
+import React, { View, Image, Text, Fragment } from 'react';
+
+// help: [ https://stackoverflow.com/questions/41064718/how-to-create-an-new-instance-of-an-object-in-react ] - precisely what is needed
+// help: [ https://create-react-app.dev/docs/importing-a-component/ ] - fix import
+// help: [ https://www.g2i.co/blog/react-class-components-with-es6-and-class-fields ] - looking for ways to define `title`,`image`,`url`,`text`
+
+// help: [ https://www.robinwieruch.de/react-class-component-deprecated/ ]
+// class Card extends React.Component {
+
+// help: [ https://www.robinwieruch.de/react-function-component/#react-function-component-example ]
+const Card = (props) => {
+
+    // constructor(props) {
+        // super(props) // help: [ https://stackoverflow.com/questions/31067368/how-to-extend-a-class-without-having-to-use-super-in-es6 ]
+        console.log(props) // help: [ https://stackoverflow.com/questions/40433463/what-does-calling-super-in-a-react-constructor-do ]
+        // this.state = {
+        //     value: null,
+        //   }
+        // todo: by default display `title` and `image`
+        // todo: .onMouseHover() display the `text` in scrollable box
+        // todo: .onMouseCick() open a new tab with the `url` displaying the object in detail
+
+        // help: [ https://www.digitalocean.com/community/tutorials/react-constructors-with-react-components ]
+        // this.onClick = this.onClick.bind(this);
+        // this.onHover = this.onHover.bind(this);
+    // }
+
+    // help: [ https://reactjs.org/docs/react-component.html ] - instead of reconstructing object
+    // componentDidUpdate(prevProps, prevState, snapshot) {
+
+    // }
+
+    // render() {
+      // help: [ https://stackoverflow.com/questions/33766085/how-to-avoid-extra-wrapping-div-in-react ]
+        return (
+          <Text>Hello !</Text>
+          // <Fragment>
+          //   <Text>Hello, I am your cat!</Text>
+          //   <div className="card">
+          //     <View>
+          //       {/* <Image
+          //         source={{uri: "https://reactnative.dev/docs/assets/p_cat1.png"}}
+          //         style={{width: 200, height: 200}}
+          //       /> */}
+          //       <Text>Hello, I am your cat!</Text>
+          //     </View>  
+          //     {/* <Image src="" /> */}
+          //     {/* <Text>{this.props.title}</Text> */}
+          //   </div>
+          // </Fragment>
+        );
+      // }
+}
+
+// help: [ https://reactnative.dev/docs/intro-react ] - why getting error
+export default Card; // help: [ https://stackoverflow.com/questions/36426521/what-does-export-default-do-in-jsx ] - this should work, but getting "type is invalid -- expected a string (for built-in components) or a class/function (for composite components) but got: undefined. You likely forgot to export your component from the file it's defined in, or you might have mixed up default and named imports."
