41ca16534ef8b4b32f3b025d05eced5ab09b1b10
Mihai Moldovan <mihaiflorinmoldovan@yahoo.com>
2022-09-07 15:38:22 +0300

    fix(logger): suppresed logs using level of importance

--- showcase-ideas/src/helpers/logger.js
+++ showcase-ideas/src/helpers/logger.js
@@ -1,8 +1,21 @@
+let LOG_LEVEL = "info"
+
+const log_levels = {
+    'debug': 0,
+    'info': 1,
+    'warning': 2,
+    'error': 3,
+    'exception': 4,
+    'critical': 5
+}
+
 const log = (level, message, object) => {
     // todo: implement log levels, have a global variable set up, and depending on its value show logs
     // todo: add a way to track where the printer was called, and write the filename and line number -> speeds up debugging a lot
-    console.log(message + ": " + object)
-    console.log(message + ": " + JSON.stringify(object, null, 2))
+    if (log_levels[LOG_LEVEL] <= log_levels[level]) {
+        console.log(message + ": " + object)
+        console.log(message + ": " + JSON.stringify(object, null, 2))
+    }
 }
 
 export default log;
